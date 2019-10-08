4285b78d760dd6b6bf60fa972c8340f3a5b7153d
NumbersZeros <thotpbank050699@gmail.com>
2019-10-08 19:53:44 +0700

    change project

---
+++ PharmacySystem/PharmacySystem/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <View> 
+          <Text>Hello thanh tho</Text>
+        </View>
+        <View> 
+          <Text>Hello ngoc hien</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
