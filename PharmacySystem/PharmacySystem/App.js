86e4741df3e6d3631c9ba06b2a8ea291adcb92e0
NumbersZeros <thotpbank050699@gmail.com>
2019-10-10 08:20:31 +0700

    Thêm tài liệu tham khảo cho ứng dụng

--- PharmacySystem/PharmacySystem/App.js
+++ PharmacySystem/PharmacySystem/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View>
-        <View> 
-          <Text>Hello thanh tho</Text>
-        </View>
-        <View> 
-          <Text>Hello ngoc hien</Text>
+export default class App extends Component {
+  render() {
+    return (
+
+      <View style={styles.container}>
+        <View>
+          <View style={styles.btn}>
+            <Button  title='Thanh tho' />
+          </View>
+          <View>
+            <Text>Hello ngoc hien</Text>
+          </View>
+          <View>
+            <Text>Hello ngoc hien</Text>
+          </View>
+          <View>
+            <Text>Hello ngoc hien</Text>
+          </View>
         </View>
       </View>
-    </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center"
   },
+  btn:{
+    height: 100,
+    width: 300,
+  }
 });
