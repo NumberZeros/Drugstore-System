827ce2f8ca087fbd196eae9a50a8690723364d15
NumberZeros <thotpbank050699@gmail.com>
2019-10-28 16:13:50 +0700

    sửa lỗi file android

--- PharmacySystem/AppNavigation.js
+++ PharmacySystem/AppNavigation.js
@@ -1,14 +1,14 @@
 import {createStackNavigator} from 'react-navigation-stack';
 import Home from './screen/Home';
-import Caterogies from './screen/caterogies';
+import Caterogies from './screen/catergories';
 const RootStack = createStackNavigator(
   {
     Home: {
-      screen:Home,
+      screen: Home,
     },
     Details: {
-      screen:Caterogies,
-    }
+      screen: Caterogies,
+    },
   },
   {
     initialRouteName: 'Home',
